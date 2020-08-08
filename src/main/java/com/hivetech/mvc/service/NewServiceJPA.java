4349d1b2b51ed6b5e88faf020b42c0fd1d27a5b7
DoanHieu <doanhieu.bl.hn@gmail.com>
2020-08-08 11:25:45 +0700

    ban config JPA

--- src/main/java/com/hivetech/mvc/service/NewServiceJPA.java
+++ src/main/java/com/hivetech/mvc/service/NewServiceJPA.java
@@ -1,9 +1,11 @@
 package com.hivetech.mvc.service;
 
 import com.hivetech.mvc.entity.NewEntity;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 
-
+@Service
 public interface NewServiceJPA {
     List<NewEntity> finaAll();
 }
