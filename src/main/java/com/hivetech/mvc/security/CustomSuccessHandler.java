32d67aefd422cd395079b9d820938a072aee1d2a
DoanHieu <doanhieu.hdv@gmail.com>
2020-08-21 00:35:00 +0700

    phan6

--- src/main/java/com/hivetech/mvc/security/CustomSuccessHandler.java
+++ src/main/java/com/hivetech/mvc/security/CustomSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.hivetech.mvc.security;
 
+import com.hivetech.mvc.util.SecurityUtils;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
@@ -9,16 +10,17 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @Component
 public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
-    
+
     private RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
             throws IOException {
-        String targetUrl =determineTargetUrl(authentication); //xu ly vao man hinh nao
+        String targetUrl = determineTargetUrl(authentication); //xu ly vao man hinh nao
         if (response.isCommitted()) {
             return;
         }
@@ -26,8 +28,40 @@ public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     }
 
     private String determineTargetUrl(Authentication authentication) {
-        String url ="";
-
+        String url = "";
+        // ph?n quy?n redirect ra các trang theo role
+        // if role is ADMIN  so redirect to controller : /quan-tri/trang-chu
+        //if role is USER so redirect to controller :/trang-chu
+        List<String> roles = SecurityUtils.getAuthorities();
+        if (isAdmin(roles)) {
+            url = "/quan-tri/trang-chu";
+        } else if (isUser(roles)) {
+            url = "/trang-chu";
+        }
         return url;
+    }
+
+    private boolean isAdmin(List<String> roles) {
+        if (roles.contains("ADIMN")) {
+            return true;
+        }
+        return false;
+    }
+
+    private boolean isUser(List<String> roles) {
+        if (roles.contains("USER")) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public RedirectStrategy getRedirectStrategy() {
+        return redirectStrategy;
+    }
+
+    @Override
+    public void setRedirectStrategy(RedirectStrategy redirectStrategy) {
+        this.redirectStrategy = redirectStrategy;
     }
 }
--- src/main/java/com/hivetech/mvc/service/impl/CustomUserDetailsService.java
+++ src/main/java/com/hivetech/mvc/service/impl/CustomUserDetailsService.java
@@ -1,13 +1,21 @@
 package com.hivetech.mvc.service.impl;
 
 import com.hivetech.mvc.constant.SystemConstant;
+import com.hivetech.mvc.dto.MyUser;
+import com.hivetech.mvc.entity.RoleEntity;
 import com.hivetech.mvc.entity.UserEntity;
 import com.hivetech.mvc.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -15,11 +23,19 @@ public class CustomUserDetailsService implements UserDetailsService {
     UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //lay thông tin c?a ng??i dùng d?a vào các tr??ng t?o ta 1 method findOneByUserNameAndStatus
         UserEntity userEntity = userRepository.findOneByUserNameAndStatus(username, SystemConstant.ACTIVE_STATUS);
         if(userEntity == null){
             throw new UsernameNotFoundException("User not found");
         }
-        //put
-        return null;
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for(RoleEntity role :userEntity.getRoles()){
+            authorities.add(new SimpleGrantedAuthority(role.getCode()));
+        }
+        //put thông tin sercurity duy tri h? th?ng khi use login vào
+        MyUser myUser = new MyUser(userEntity.getUserName(), userEntity.getPassword(),
+                true, true, true, true, authorities);
+        myUser.setFullName(userEntity.getFullName());
+        return myUser ;
     }
 }
